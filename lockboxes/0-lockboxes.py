4479ff8506f78a1109408d35c159f6631e4390cc
ananas122 <122387449+ananas122@users.noreply.github.com>
2024-01-05 21:59:18 +0100

    Write a method that determines if all the boxes can be opened.

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -3,6 +3,19 @@
     """
 
 def canUnlockAll(boxes):
+ """
+    Determines if all the boxes can be unlocked using the provided keys.
+
+    Args:
+    - boxes: A list of lists. Each sublist represents a box containing keys
+     to open other boxes.
+
+    Returns:
+    - True if all boxes can be unlocked, else False.
+    """
+
+ '''Check if the list is empty or null (no boxes to open)'''
+
     boxopen = {0}  # Commence avec la boîte 0 ouverte
     clés = boxes[0]  # Prend les clés de la première boîte
 
