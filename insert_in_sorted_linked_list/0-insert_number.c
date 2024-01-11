7d70109b972bb64cdbbb12347e94e040cbec3926
ananas122 <122387449+ananas122@users.noreply.github.com>
2024-01-11 15:26:27 +0100

    task 1 correction

--- insert_in_sorted_linked_list/0-insert_number.c
+++ insert_in_sorted_linked_list/0-insert_number.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+
 #include <stdlib.h>
 #include "lists.h"
 
@@ -20,8 +20,7 @@ listint_t *insert_node(listint_t **head, int number)
 		return (NULL);
 
 	new->n = number;
-	new->next = NULL;
-
+	
 	if (*head == NULL || (*head)->n >= new->n)
 	{
 		new->next = *head;
