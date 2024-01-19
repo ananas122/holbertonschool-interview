ab1368f102f3f533264a90a5f55fc700b26f1405
ananas122 <122387449+ananas122@users.noreply.github.com>
2024-01-19 19:55:40 +0100

    heap

--- heap_insert/binary_trees.h
+++ heap_insert/binary_trees.h
@@ -2,9 +2,8 @@
 #define _BINARY_TREES_H_
 
 #include <stddef.h>
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <stdio.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -14,18 +13,28 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-struct binary_tree_s
+typedef struct binary_tree_s
 {
 	int n;
 	struct binary_tree_s *parent;
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
-};
+} binary_tree_t;
 
-typedef struct binary_tree_s binary_tree_t;
-typedef struct binary_tree_s heap_t;
 void binary_tree_print(const binary_tree_t *);
+
+/* Max binary heap */
+typedef struct binary_tree_s heap_t;
+
+/* create a new node prototype */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
+/* insert value into max binary heap */
 heap_t *heap_insert(heap_t **root, int value);
+
+
+/* size of a binary tree */
+size_t binary_tree_size(const binary_tree_t *tree);
+
 
 #endif /* _BINARY_TREES_H_ */
